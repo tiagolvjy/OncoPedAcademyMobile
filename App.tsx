38b0d04dda3f45f13e2cd7c12f9571095102c75b
tiagolvjy <tiagocouto_@outlook.com>
2026-04-08 21:31:21 -0300

    Rotas Configuradas

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './src/screens/LoginScreen';
+import CadastroTipoScreen from './src/screens/CadastroTipoScreen';
+import CadastroAlunoScreen from './src/screens/CadastroAlunoScreen';
+import CadastroMedicoScreen from './src/screens/CadastroMedicoScreen';
+import EsqueciSenhaScreen from './src/screens/EsqueciSenhaScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Login"
+                screenOptions={{
+                    headerStyle: { backgroundColor: '#4703D0' },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: { fontWeight: 'bold' },
+                }}
+            >
+                <Stack.Screen
+                    name="Login"
+                    component={LoginScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="EsqueciSenha"
+                    component={EsqueciSenhaScreen}
+                    options={{ title: 'Recuperar senha' }}
+                />
+                <Stack.Screen
+                    name="CadastroTipo"
+                    component={CadastroTipoScreen}
+                    options={{ title: 'Criar conta' }}
+                />
+                <Stack.Screen
+                    name="CadastroAluno"
+                    component={CadastroAlunoScreen}
+                    options={{ title: 'Cadastro de Aluno' }}
+                />
+                <Stack.Screen
+                    name="CadastroMedico"
+                    component={CadastroMedicoScreen}
+                    options={{ title: 'Cadastro de Médico' }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
