3ce8f6604b3aaa6a7fdf309461811f7062123533
Tiago Couto <tiagocouto_@outlook.com>
2026-06-16 11:29:17 -0300

    feat: adicionada configuração padrão das variáveis de ambiente

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCxsPRvmtGFkvPDRfKiYbwyGnkBDNpxB4A",
-    authDomain: "oncoped-academy.firebaseapp.com",
-    projectId: "oncoped-academy",
-    storageBucket: "oncoped-academy.firebasestorage.app",
-    messagingSenderId: "900080691110",
-    appId: "1:900080691110:web:16a2c3d703fcc6c1229167"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
